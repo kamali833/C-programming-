3b5999079ee0e23c08f7b8afae1fdda66c60d76d
kamali833 <77154468+kamali833@users.noreply.github.com>
2021-01-13 12:58:23 +0530

    Update Vowels.c

--- Vowels.c
+++ Vowels.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 int main()
-  {
+{
   	
          int i,len,count=0;
          char name[100];
@@ -10,15 +10,17 @@ int main()
          gets(name);
          len=strlen(name);
          for(i=0;i<len;i++)
-
-         {
-               if(name[i]=='A'|| name[i]=='a'|| name[i]=='E'|| name[i]=='e'|| name[i]=='I'|| name[i]=='i'|| name[i]=='O'|| name[i]=='o'|| name[i]=='U'|| name[i]=='u' )
     
-               {
+         {
+    
+    
+                  if(name[i]=='A'|| name[i]=='a'|| name[i]=='E'|| name[i]=='e'|| name[i]=='I'|| name[i]=='i'|| name[i]=='O'|| name[i]=='o'|| name[i]=='U'|| name[i]=='u' )
+    
+                  {
 
-                    count++;
+                           count++;
       
-               }
+                  }
 
          }
 
@@ -26,4 +28,4 @@ int main()
       
          return 0;
 
-  }
+}
